7b379ebc5bec41ff5e2c29c78b09d575b5ebd35c
Daniel Molloy <danielmolloy_6@icloud.com>
2022-05-21 19:57:35 +0100

    bug fixes

--- components/playlist.tsx
+++ components/playlist.tsx
@@ -9,7 +9,7 @@ export default function Playlist(props: any) {
         {props.videoArr.map((i: {src: string, img: string}) => (
           <button className={props.videoArr[props.indexNum].src === i ? "playlist-item bg-blue-100" : "playlist-item"} onClick={() => props.setUp(props.videoArr.indexOf(i))} key={i.src}>
             <p>{props.getName(i.src)}</p>
-            <Image src={i.img} width={642/3} height={362/3}/>
+            <Image alt="Video thumbnail" src={i.img} width={642/3} height={362/3}/>
             </button>
           ))}
         </div>
